dbbc1b0ddbd9a39066f00d5c61bb7cfd942d37ca
mahdi parivash <mahdiparivash@protonmail.com>
2022-04-14 10:06:49 +0430

    Header Mobile Section Complate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,6 +30,17 @@ function Header() {
       setIsMenu(!isMenu);
     }
   };
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
+
   return (
     <header className="fixed z-50 p-1 w-screen px-4 md:p-6 md:px-16">
       {/* desktop */}
@@ -86,7 +97,10 @@ function Header() {
                 </Link>
               )}
 
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer  hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor">
+              <p
+                onClick={logout}
+                className="px-4 py-2 flex items-center gap-3 cursor-pointer  hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor"
+              >
                 LogOut <MdLogout />
               </p>
             </motion.div>
@@ -94,11 +108,64 @@ function Header() {
         </div>
       </div>
       {/* Mobile */}
-      <div className="flex md:hidden h-full  p-4">
+      <div className="flex md:hidden items-center justify-between h-full  p-4">
+        <div className="relative flex items-center justify-center">
+          <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+          <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+        </div>
         <Link to="/" className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="Logo" />
           <p className="text-xl text-headingColor font-semibold  ">City </p>
         </Link>
+
+        <div className="relative ">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : Avatar}
+            className="w-9 h-9 cursor-pointer rounded-full drop-shadow-xl"
+            alt="avatar"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl top-[42px] right-0 rounded-lg flex flex-col absolute "
+            >
+              {user && user.email === "random@gmail.com" && (
+                <Link>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer  hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor">
+                    New Item <MdAdd />
+                  </p>
+                </Link>
+              )}
+              <ul className="flex flex-col">
+                <li className="text-base px-4 py-2 text-textColor cursor-pointer transition-all ease-out hover:text-headingColor  hover:bg-slate-100 ">
+                  Home
+                </li>
+                <li className="text-base px-4 py-2 text-textColor cursor-pointer transition-all ease-out hover:text-headingColor  hover:bg-slate-100 ">
+                  Menu
+                </li>
+                <li className="text-base px-4 py-2 text-textColor cursor-pointer transition-all ease-out hover:text-headingColor  hover:bg-slate-100 ">
+                  About Us
+                </li>
+                <li className="text-base px-4 py-2 text-textColor cursor-pointer transition-all ease-out hover:text-headingColor  hover:bg-slate-100 ">
+                  Service
+                </li>
+              </ul>
+
+              <p
+                onClick={logout}
+                className="m2 bg-gray-200 p-2 hover:bg-gray-300 rounded-md shadow-md flex items-center gap-3 cursor-pointer justify-center transition-all duration-100 ease-in-out text-textColor"
+              >
+                LogOut <MdLogout />
+              </p>
+            </motion.div>
+          )}
+        </div>
       </div>
     </header>
   );
