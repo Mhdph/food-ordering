0567e006de941252034166a06b0681cc34033068
mahdi parivash <mahdiparivash@protonmail.com>
2022-05-19 09:21:20 +0430

    Create Main Container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomeContainer from "./HomeContainer";
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import RowContainer from "./RowContainer";
+import { useStateValue } from "../context/StateProvider";
 
 function MainContainer() {
+  const [{ foodItems }, dispatch] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {}, [scrollValue]);
+
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
       <HomeContainer />
+      <section className="w-full my-6">
+        <div className="flex w-full items-center justify-between">
+          <p className="text-2xl font-semibold text-headingColor capitalize relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-4 before:left-0 before:bg-orange-500 transition-all ease-in-out duration-100">
+            Our fresh & healthy fruits
+          </p>
+          <div className="hidden items-center md:flex gap-3">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 flex items-center justify-center hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"
+              onClick={() => setScrollValue(-200)}
+            >
+              <MdChevronLeft className="text-lg " />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 flex items-center justify-center hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"
+              onClick={() => setScrollValue(200)}
+            >
+              <MdChevronRight className="text-lg " />
+            </motion.div>
+          </div>
+        </div>
+        <RowContainer
+          scrollValue={scrollValue}
+          flag={true}
+          data={foodItems?.filter((n) => n.categories === "fruits")}
+        />
+      </section>
     </div>
   );
 }
